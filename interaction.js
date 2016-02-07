b0db009ed3012009c58f65fabfb12fe9a3dccf4f
PaulineF <flambard.pauline@laposte.net>
2016-02-07 17:00:21 +0100

    Interaction

--- interaction.js
+++ interaction.js
@@ -3,10 +3,51 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+    this.initialX = 0;
+    this.initialY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.isPressed = false;
 
 	// Developper les 3 fonctions gérant les événements
+    this.onPress = function (evt){
+        if(!this.isPressed){
+            this.isPressed = true;
+            var pos = getMousePosition(canvas, evt);
+            this.initialX = pos.x;
+            this.initialY = pos.y;
+            console.log("initialX : " + this.initialX +" // initialY : " + this.initialY);
+        }
+    }.bind(this);
+
+    this.onMove = function(evt){
+        if(this.isPressed){
+            var pos = getMousePosition(canvas, evt);
+            this.finalX = pos.x;
+            this.finalY = pos.y;
+            console.log("Move");
+
+            //console.log("initialX : " + this.initialX +" // initialY : " + this.initialY);
+            console.log("finalX : " + this.finalX +" // finalY : " + this.finalY);
+        }
+    }.bind(this);
+
+
+    this.onUp = function (evt){
+        if(this.isPressed){
+            this.isPressed = false;
+            var pos = getMousePosition(canvas, evt);
+            this.finalX = pos.x;
+            this.finalY = pos.y;
+
+            console.log("finalX : " + this.finalX +" // finalY : " + this.finalY);
+        }
+    }.bind(this);
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousedown', this.onPress, false);
+    canvas.addEventListener('mousemove', this.onMove, false);
+    canvas.addEventListener('mouseup', this.onUp, false);
 };
 
 
