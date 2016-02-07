fdb6d396aff5a8249460577a5d3d0b0549a2a987
PaulineF <flambard.pauline@laposte.net>
2016-02-07 17:12:53 +0100

    model

--- model.js
+++ model.js
@@ -1,3 +1,28 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+ function Form(couleur, epaisseur ){
+     this.couleur=couleur;
+     this.epaisseur=epaisseur;
+ }
+
+function Line(couleur, epaisseur, startX, startY, endX, endY){
+    Form.call(this, couleur, epaisseur);
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+
+}
+
+function Rectangle(couleur, epaisseur, startX, startY, longueur, largeur){
+    Form.call(this, couleur, epaisseur);
+    this.startX = startX;
+    this.startY = startY;
+    this.longueur = longueur;
+    this.largeur = largeur;
+}
+
+function Drawing(){
+    this.forms = new Array();
+}
